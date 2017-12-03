392916c932e2f9645f4ee52202cb129aad146c18
Chocco-Crokko <zastiralkin@gmail.com>
2017-12-03 16:18:21 +0300

    tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,31 @@
 #include "gtest/gtest.h"
+#include "../src/superclass.cpp"
 
 namespace {
 
-TEST(Lol, kek) {
-    ASSERT_EQ(2 + 2, 4);
+TEST(board3x3, kek1) {
+    chessy::parse("../test/data/3x3/001");
+    std::vector<chessy::i_solution> solutions = chessy::solve();
+    ASSERT_EQ(2, solutions.size());
 }
+
+TEST(board3x3, kek2) {
+    chessy::parse("../test/data/3x3/002");
+    std::vector<chessy::i_solution> solutions = chessy::solve();
+    ASSERT_EQ(0, solutions.size());
+}
+
+//TEST(board3x3AAA, kek3) {
+//    chessy::parse("../test/data/3x3/003");
+//    std::vector<chessy::i_solution> solutions = chessy::solve();
+//    ASSERT_EQ(1, solutions.size());
+//}
+
+//TEST(board3x3, kek4) {
+//    chessy::parse("../test/data/3x3/004");
+//    std::vector<chessy::i_solution> solutions = chessy::solve();
+//    ASSERT_EQ(0, solutions.size());
+//}
 
 }
 
