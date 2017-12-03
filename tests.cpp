7fd7085f3c8badf12601eba56b1a86079922fb6a
Chocco-Crokko <zastiralkin@gmail.com>
2017-12-03 15:23:19 +0300

    try travis

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,14 @@
 #include "gtest/gtest.h"
 
+namespace {
+
 TEST(Lol, kek) {
     ASSERT_EQ(2 + 2, 4);
+}
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
